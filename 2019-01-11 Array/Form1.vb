537f1d04b5c72b1102d761b8a7767e1a6276e602
Benedikt Michael Schneider <bs762448@fh-muenster.de>
2019-02-13 11:48:57 +0100

    Test

--- 2019-01-11 Array/Form1.vb
+++ 2019-01-11 Array/Form1.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim arrTest(3, 2) As Integer
+        Dim mSum As Integer
+        Randomize()
+        For i = 0 To 3 Step 1
+            For j = 0 To 2 Step 1
+                arrTest(i, j) = Int(Rnd() * 9 + 1)
+                mSum = mSum + arrTest(i, j)
+                MsgBox(arrTest(i, j))
+            Next
 
+        Next
+            MsgBox(mSum)
+
+
+    End Sub
 End Class
